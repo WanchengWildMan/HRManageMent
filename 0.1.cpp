d946eebafc721d67d29e5e43b38da29e451bd868
WanchengWildMan <1150327966@qq.com>
2020-12-22 21:37:11 +0800

    12.22

--- 0.1.cpp
+++ 0.1.cpp
@@ -74,11 +74,12 @@ class PartTimeSalesMan : public PartTimeWorker {
  private:
   /* data */
  public:
+ double SalesVolume;
   PartTimeSalesMan(/* args */);
   PartTimeSalesMan(Person);
   ~PartTimeSalesMan();
   void UpdateLevel();
-  void
+  double GetMoney();//销售额提成
 };
 
 template <typename T>
@@ -93,6 +94,7 @@ class Table {
   void insert(T);
   void remove(T);  //根据
   void modify();
+  string query();
 };
 template <typename T>
 Table ::insert(T peo) {
@@ -101,6 +103,7 @@ Table ::insert(T peo) {
   table[i + 1] = peo;
   num++;
 }
+template <typename T>
 Table ::remove(T peo) {
   int id = peo.id;
   int pos = lower_bound(table + 1, table + 1 + num, id) - table;
@@ -111,9 +114,9 @@ Table ::remove(T peo) {
 }
 Table ::remove(int id) {
   int pos = lower_bound(table + 1, table + 1 + num, id) - table;
-  if(table[pos].id!=id){
-    cout<<"未找到工号为"<<id<<"的员工！"<<endl;
-    return ;
+  if (table[pos].id != id) {
+    cout << "未找到工号为" << id << "的员工！" << endl;
+    return;
   }
   for (int i = pos; i < num; i--) {
     table[i] = table[i + 1];
@@ -121,9 +124,12 @@ Table ::remove(int id) {
   num--;
 }
 Table::remove(string name) {
-  bool ok=0;
-  for(int i=1;i<=num;i++){
-    if(table[i].name==name){ok=1;break;}
+  bool ok = 0;
+  for (int i = 1; i <= num; i++) {
+    if (table[i].name == name) {
+      ok = 1;
+      break;
+    }
   }
   if (!ok) {
     cout << "未找到姓名为" << name << "的员工！" << endl;
@@ -135,7 +141,7 @@ class TotalTable {
   Table<Manager> Group_Manager;
   Table<PartTimeTechnologyer> Group_PartTimeTechnologyer;
   Table<SaleManager> Group_SaleManager;
-  Table<PartTimeSalesMan>Group_PartTimeSalesMan;
+  Table<PartTimeSalesMan> Group_PartTimeSalesMan;
   void insert(T, int);  //第二个参数标识类别
   void remove(int);     //根据id
   void remove(string);  //根据人名
